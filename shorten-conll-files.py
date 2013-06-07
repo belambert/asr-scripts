1b6779a5a9f4a1a0c7e30994479f6b13e97802ac
Benjamin Lambert <ben@benjaminlambert.com>
2013-06-07 18:12:56 -0400

    don't print so much stuff

--- shorten-conll-files.py
+++ shorten-conll-files.py
@@ -10,15 +10,10 @@ max_length = 100
 def shorten_conll_file(filename):
     assert (filename.endswith('.gz'))
     new_filename = "%s.%d.gz"%(filename[:-3], max_length)
-    print "Working on file: %s"%filename
     file = gzip.open(filename)
     new_file = gzip.open(new_filename, 'wb')
-    counter = 0
-    
     count = 0
     for line in file:
-        #line = line[:-1]
-        print count
         if line == "\n":
             count += 1
         if count == max_length:

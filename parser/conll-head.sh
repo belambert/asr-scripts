7a0ddfb1901c8ac1384b46a52ec7db813f4a91c6
Benjamin Lambert <ben@benjaminlambert.com>
2013-06-14 17:28:25 -0400

    bugfixes to conll head and tail script

--- parser/conll-head.sh
+++ parser/conll-head.sh
@@ -2,12 +2,12 @@
 
 import sys
 
-if len(sys.argv != 3):
+if len(sys.argv) != 3:
     print "%s -n <filename>"%sys.argv[0]
     exit(1)
 
-file = sys.argv[1]
-count_target = int(sys.argv[2][1:])
+count_target = int(sys.argv[1][1:])
+file = sys.argv[2]
 count = 0
 
 for line in open(file):

5140c3bec8bd6e1ebdfa4f28161af8836b1bec63
Benjamin Lambert <ben@benjaminlambert.com>
2013-06-14 17:24:38 -0400

    renaming and fixing interface to scripts to get conll head and tail

--- parser/conll-tail.sh
+++ parser/conll-tail.sh
@@ -2,9 +2,16 @@
 
 import sys
 
+if len(sys.argv != 3):
+    print "%s -n <filename>"%sys.argv[0]
+    exit(1)
+
 file = sys.argv[1]
-count_target = int(sys.argv[2])
+count_target = int(sys.argv[2][1:])
 count = 0
+
+# This one is a little more complex than conll-head.sh... because
+# we have to count from the end.
 lines = open(file).readlines()
 lines_to_print = []
 
